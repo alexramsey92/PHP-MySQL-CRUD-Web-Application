85c913560bf6c2d559783d56de6989282348f1cd
Alex Ramsey <alexramsey92@gmail.com>
2024-08-18 21:17:29 -0400

    added docker but its wip

--- api/config/database.php
+++ api/config/database.php
@@ -1,26 +1,29 @@
 <?php
-class Database{
- 
-    // specify your own database credentials
-    private $host = "localhost";
-    private $db_name = "hospital_db";
-    private $username = "root";
-    private $password = "";
-    public $conn;
- 
-    // get the database connection
-    public function getConnection(){
- 
+
+class Database {
+    private $host = '';
+    private $db_name = '';
+    private $username = '';
+    private $password = '';
+    private $conn;
+
+    public function __construct() {
+        $this->host = getenv('DB_HOST');
+        $this->db_name = getenv('DB_NAME');
+        $this->username = getenv('DB_USER');
+        $this->password = getenv('DB_PASSWORD');
+    }
+
+    public function getConnection() {
         $this->conn = null;
- 
-        try{
-            $this->conn = new PDO("mysql:host=" . $this->host . ";dbname=" . $this->db_name, $this->username, $this->password);
-            $this->conn->exec("set names utf8");
-        }catch(PDOException $exception){
+
+        try {
+            $this->conn = new PDO("mysql:host={$this->host};dbname={$this->db_name}", $this->username, $this->password);
+            $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        } catch(PDOException $exception) {
             echo "Connection error: " . $exception->getMessage();
         }
- 
+
         return $this->conn;
     }
 }
-?>
